a39b15e9b8a5050f3bd66943eded607443f6b5ba
Blair Bristow <blairhbristow@gmail.com>
2022-03-25 12:25:21 -0400

    Add function 1

--- exercise.js
+++ exercise.js
@@ -1,6 +1,13 @@
+function getTotalItemSales(sales, itemId){
+    let itemSales =  sales.filter(sale => sale.itemId === itemId)
+    let itemQuantities = itemSales.map(sale => sale.quantity)
+    return itemQuantities.reduce((sum,number)=> sum + number,0)
+}
+
+
 module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
-    addTotalSalesToItems,
-    addTotalValueToItems
+    //addTotalSalesToItems,
+    //addTotalValueToItems
 }
